72b5cb537b8eda8d9f2023fccc2ebee06108311c
Taylor Griffin <tmgriffin73@gmail.com>
2022-05-11 22:56:59 -0400

    add roll dice button

--- App.js
+++ App.js
@@ -1,36 +1,30 @@
-import React from "react"
-import Die from "./Die"
-
-/**
- * Challenge:
- * 
- * Create state to hold our array of numbers. (Initialize
- * the state by calling our `allNewDice` function so it 
- * loads all new dice as soon as the app loads)
- * 
- * Map over the state numbers array to generate our array
- * of Die elements and render those in place of our
- * manually-written 10 Die elements.
- */
+import React from 'react';
+import Die from './Die';
 
 export default function App() {
-    const [dice, setDice] = React.useState(allNewDice())
-    
-    function allNewDice() {
-        const newDice = []
-        for (let i = 0; i < 10; i++) {
-            newDice.push(Math.ceil(Math.random() * 6))
-        }
-        return newDice
+  const [dice, setDice] = React.useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+      });
     }
-    
-    const diceElements = dice.map(die => <Die value={die} />)
-    
-    return (
-        <main>
-            <div className="dice-container">
-                {diceElements}
-            </div>
-        </main>
-    )
+    return newDice;
+  }
+
+  const diceElements = dice.map(({ value, isHeld }) => (
+    <Die value={value} isHeld={isHeld} />
+  ));
+
+  return (
+    <main>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={() => setDice(allNewDice())}>
+        Roll Dice
+      </button>
+    </main>
+  );
 }
